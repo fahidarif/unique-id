0fc27dfd9316b6fd66913be12a5052800508d168
M Fahad <fahidarif@gmail.com>
2023-12-02 04:03:01 +0500

    fix typescrip types

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,37 @@
 /**
  * @return {string} Returns The Unique ID.
  */
-declare function get(): string;
+export function get(): string;
 
 /**
  * @param {string} unique64Characters Input of 64 unique characters which you want to use in id generation.
  */
-declare function characters(unique64Characters: string): { message: string, status: boolean };
+export function characters(unique64Characters: string): {
+    message: string;
+    status: boolean;
+} | {
+    status: boolean;
+    message?: undefined;
+};
 
 /**
  * @param {string} prefixInput Input of prefix, Max length can be 50 chars.
  */
-declare function setPrefix(prefixInput: string): { message: string, status: boolean };
+export function setPrefix(prefixInput: string): {
+    message: string;
+    status: boolean;
+} | {
+    status: boolean;
+    message?: undefined;
+};
 
 /**
  * @param {string} postfixInput Input of postfix, Max length can be 50 chars.
  */
-declare function setPostfix(postfixInput: string): { message: string, status: boolean };
-
-export { get, characters, setPrefix, setPostfix };
+export function setPostfix(postfixInput: string): {
+    message: string;
+    status: boolean;
+} | {
+    status: boolean;
+    message?: undefined;
+};
