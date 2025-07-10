584a50fbad5e047d2e37ba76fc3116243719f763
M Fahad <>
2025-07-11 03:24:09 +0500

    update typings

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,12 @@
 /**
  * @return {string} Returns The Unique ID.
  */
-export function get(): string;
+export function get(prefix?: string, suffix?: string): string;
 
 /**
  * @param {string} unique64Characters Input of 64 unique characters which you want to use in id generation.
  */
 export function characters(unique64Characters: string): {
-    message: string;
-    status: boolean;
-} | {
-    status: boolean;
-    message?: undefined;
-};
-
-/**
- * @param {string} prefixInput Input of prefix, Max length can be 50 chars.
- */
-export function setPrefix(prefixInput: string): {
-    message: string;
-    status: boolean;
-} | {
-    status: boolean;
-    message?: undefined;
-};
-
-/**
- * @param {string} postfixInput Input of postfix, Max length can be 50 chars.
- */
-export function setPostfix(postfixInput: string): {
     message: string;
     status: boolean;
 } | {
